75c4dec9bac6e8db409fe3a4020edbd536629d39
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-03 16:58:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog
+
+  attr_accessor = :name
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.push(name)
+  end
+
+  def save
+
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
